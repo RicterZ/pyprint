e6ec0fef16b54bfb7d72094c0563b55d475891d5
RicterZ <RicterZheng@gmail.com>
2014-01-26 22:57:01 +0800

    a better settings

--- lib/settings.py
+++ lib/settings.py
@@ -1,12 +1,14 @@
+import web
+import jinja2 as jj
+
 MySQL_host = 'localhost'
 MySQL_user = 'root'
 MySQL_pass = ''
 MySQL_DB = ''
+web.config.debug = False
+#if you are a developer, you should set web.config.debug = True
 
 from local_settings import *
-import web
-import jinja2 as jj
 
-web.config.debug = False
 db = web.database(host=MySQL_host, dbn='mysql', db=MySQL_DB, user=MySQL_user, pw=MySQL_pass)
 env = jj.Environment(loader=jj.FileSystemLoader('templates'))
