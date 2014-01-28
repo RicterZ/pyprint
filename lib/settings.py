237165de593ca69d1d58335b2649b37f177ed9cb
RicterZ <canyuexiaolang@gmail.com>
2014-01-28 11:58:26 +0800

    Update settings.py

--- lib/settings.py
+++ lib/settings.py
@@ -1,8 +1,8 @@
 import web
 import jinja2 as jj
 
-# database_type = 'MySQL' or 'sqlite'
-database_type = 'MySQL'
+# database_type = 'mysql' or 'sqlite'
+database_type = 'mysql'
 # these values is for MySQL
 MySQL_host = 'localhost'
 MySQL_user = 'root'
@@ -16,7 +16,7 @@ web.config.debug = False
 
 from local_settings import *
 
-if database_type == 'MySQL':
+if database_type == 'mysql':
 	db = web.database(host=MySQL_host, dbn='mysql', db=MySQL_DB, user=MySQL_user, pw=MySQL_pass)
 elif database_type == 'sqlite':
 	db = web.database(dbn='sqlite', db=sqlite_path)
