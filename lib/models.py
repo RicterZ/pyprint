791c4a781b79511d16c2c7e48cacf33ac861b2e7
RicterZ <RicterZheng@gmail.com>
2014-02-05 23:40:20 +0800

    fix bug of paging

--- lib/models.py
+++ lib/models.py
@@ -6,7 +6,7 @@ from utils import clean_input, now, password_to_md5, make_session, markdown_to_h
 
 def list_three_articles(page=1):
     #data = db.select('articles', where='1', limit=3, order='id desc')
-    data = db.query('select * from articles where 1 order by id desc limit %d, 3' % (page - 1))
+    data = db.query('select * from articles where 1 order by id desc limit %d, 3' % ((page - 1) * 3))
     return data
 
 
