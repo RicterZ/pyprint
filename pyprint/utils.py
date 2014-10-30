7b63cffc39a563c20115a0c42192253de7ff19f9
ricterz <ricterzheng@gmail.com>
2014-10-30 16:57:23 +0800

    add Storage class

--- pyprint/utils.py
+++ pyprint/utils.py
@@ -2,6 +2,14 @@ import re
 from markdown import markdown
 
 
+class Storage(dict):
+    def __getattr__(self, item):
+        return self[item]
+
+    def __setattr__(self, key, value):
+        self[key] = value
+
+
 def get_host(url):
     return '/'.join(url.split('/')[2:-1])
 
@@ -10,22 +18,22 @@ def posts_markdown(posts):
     if not isinstance(posts, (list,)):
         posts = [posts]
 
-    return [{
+    return [Storage({
         'title': post.title,
         'created_time': post.created_time,
         'content': markdown(post.content),
         'tags': post.tags
-    } for post in posts]
+    }) for post in posts]
 
 
 def fix_lazy_load(posts):
     if not isinstance(posts, (list,)):
         posts = [posts]
 
-    return [{
+    return [Storage({
         'title': post.title,
         'created_time': post.created_time,
         'content': re.sub(r'<img alt="(.+?)" src="" />', '<img src="\g<1>" />', post.content),
         'tags': post.tags
-    } for post in posts]
+    }) for post in posts]
 
