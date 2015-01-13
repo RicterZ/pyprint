3aed7cea7c17a226caa5e20683a046dce8fd3404
Ricter <RicterZheng@gmail.com>
2015-01-13 22:30:29 +0800

    add commandwq

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 import tornado.ioloop
 import tornado.httpserver
+import tornado.options
+from tornado.options import define, options
 from pyprint import Application
 
 
+define('port', default=8888, help='listen on the port', type=int)
+
+
 if __name__ == '__main__':
+    tornado.options.parse_command_line()
     http_server = tornado.httpserver.HTTPServer(Application())
-    http_server.listen(8888)
+    http_server.listen(options.port)
 
     tornado.ioloop.IOLoop.instance().start()
