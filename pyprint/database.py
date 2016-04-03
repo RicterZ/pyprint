5d13457aa090bd4e3a6aee7daf859f3768cb72d0
Ricter Z <ricterzheng@gmail.com>
2016-04-03 14:33:05 +0800

    add pool_recycle and update requirements

--- pyprint/database.py
+++ pyprint/database.py
@@ -5,6 +5,6 @@ from sqlalchemy import create_engine
 from settings import connect_str
 
 
-engine = create_engine(connect_str, echo=True)
+engine = create_engine(connect_str, echo=True, pool_recycle=3600)
 Base = declarative_base()
 db = scoped_session(sessionmaker(bind=engine))
